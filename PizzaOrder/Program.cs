c239739d9ca7d1693de72ef563f255afc775b4e1
Parker Adams <AdamsP25@bolles.org>
2020-12-13 15:17:14 -0500

    Latest Version

---
+++ PizzaOrder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PizzaOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var expr = true;
+            Console.Write("Hello");
+            var user = Console.ReadLine();
+            expr = user switch
+            {
+                "yes" => false,
+                _ => true,
+            };
+            if (expr == true)
+            {
+                Console.WriteLine("Hello World!");
+                Console.Write("Hello");
+
+                Console.Write("Sto");
+            }
+            else
+            {
+                Console.Clear();
+            }
+
+           
+        }
+    }
+}
